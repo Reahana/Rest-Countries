dfd2e181b38dd0b18082e41e0bd45fc76661382e
Amanda Reahana Gomes <reahana233@gmail.com>
2023-04-20 13:39:01 +0600

    option 1

--- js/countries.js
+++ js/countries.js
@@ -3,3 +3,22 @@ const loadCountries = () =>{
     .then(res => res.json())
     .then(data => displayCountries(data));
 }
+const displayCountries = countries=>{
+     // console.log(countries[0]);
+     const countriesHTML = countries.map(country => getCountryHTML(country));
+     // console.log(countriesHTML[0]);
+     const container = document.getElementById('countries');
+     container.innerHTML = countriesHTML.join(' ');
+}
+   // option 1
+const getCountryHTML = country =>{
+    return `
+        <div class="country">
+            <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+}
+
+
+loadCountries()
