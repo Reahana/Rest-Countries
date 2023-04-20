90917e9cbd6b587704a5bd9b73559d79235bb920
Amanda Reahana Gomes <reahana233@gmail.com>
2023-04-20 13:55:25 +0600

    destructuring

--- js/countries.js
+++ js/countries.js
@@ -4,21 +4,46 @@ const loadCountries = () =>{
     .then(data => displayCountries(data));
 }
 const displayCountries = countries=>{
-     // console.log(countries[0]);
+      console.log(countries[0]);
      const countriesHTML = countries.map(country => getCountryHTML(country));
      // console.log(countriesHTML[0]);
      const container = document.getElementById('countries');
      container.innerHTML = countriesHTML.join(' ');
 }
-   // option 1
-const getCountryHTML = country =>{
+// option 3
+const getCountryHTML = ({name, flags, area, region ,capital}) =>{
     return `
         <div class="country">
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}">
+            <h2>${name.common}</h2>
+            <p>Capital: ${capital}</p>
+            <p>Area: ${area}</p>
+            <p>Continet: ${region}</p>
+            <img src="${flags.png}">
         </div>
     `
 }
+
+// option 2
+// const getCountryHTML = country =>{
+//     // option 1
+//     const {name, flags} = country;
+//     return `
+//         <div class="country">
+//             <h2>${name.common}</h2>
+//             <img src="${flags.png}">
+//         </div>
+//     `
+//  }
+
+   // option 1
+// const getCountryHTML = country =>{
+//     return `
+//         <div class="country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}">
+//         </div>
+//     `
+// }
 
 
 loadCountries()
